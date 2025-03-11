70cb1843f8df51230eed3e51bbc2319953d7cce5
Brady Welsh <blwelsh@calpoly.edu>
2025-03-11 12:26:07 -0700

    finished basic cypress test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -24,8 +24,34 @@ And I shouldn’t be able to complete my sign-up without fixing my email
 */
 
 
-describe('Sign Up', () => {
-  /*
+describe('Sign In', () => {
+
+
+  context("Invalid password", () => {
+    beforeEach(() => {});
+
+    it("GIVEN I have opened up the main page", () => {
+      cy.clearCookies();
+    });
+
+    it("WHEN I enter a valid email, before entering a valid name and password", () => {
+      cy.visit('http://localhost:3000');
+      cy.get("form").within(() => {
+        cy.wait(1000);
+        cy.get('input[id="email"]').should('not.be.disabled').type("testing@gmail.com");
+        cy.get('button[type="submit"]').click();
+        cy.get('input[id="password"]').should('be.visible').type("wrongPassword!");
+        cy.get('button[type="submit"]').click();
+      });
+
+    });
+
+    it("THEN the user should stay on the same page and be unable to proceed", () => {
+      cy.get("h2").should('contain', "Welcome Back");
+    })
+
+  });
+  
   context("Successful sign-up", () => {
     beforeEach(() => {});
 
@@ -34,11 +60,10 @@ describe('Sign Up', () => {
     it("WHEN I enter a valid email, before entering a valid name and password", () => {
       cy.visit('http://localhost:3000');
       cy.get("form").within(() => {
+        cy.wait(1000);
         cy.get('input[id="email"]').should('not.be.disabled').type("testing@gmail.com");
         cy.get('button[type="submit"]').should('be.visible').click();
-        cy.get('input[id="firstName"]').type("Cypress");
-        cy.get('input[id="password"]').type("e3eiei$woFo");
-        cy.get('input[id="confirmPassword"]').type("e3eiei$woFo");
+        cy.get('input[id="password"]').should('be.visible').type("Password123$");
         cy.get('button[type="submit"]').click();
       })
     });
@@ -46,68 +71,20 @@ describe('Sign Up', () => {
     it("THEN I should see a check mark and confirmation screen, before being shown to the home page", () => {
       cy.url().should('include', '/');
 
-      cy.get('h1').should('contain', 'Welcome back, Cypress!'); // Checks if there is the home page header
+      cy.get('h1').should('contain', 'Home'); // Checks if there is the home page header
     });
   });
-  */
+  
 
-  context("Invalid email", () => {
-    beforeEach(() => {});
 
-    it("GIVEN I have opened up the main page", () => {});
-
-    it("WHEN I enter a valid email, before entering a valid name and password", () => {
-      cy.visit('http://localhost:3000');
-      cy.get("form").within(() => {
-        cy.get('input[id="email"]').type("failedmeila");
-        cy.get('button[type="submit"]').click();
-      });
-
-    });
-
-    it("THEN the user should stay on the same page and be unable to proceed", () => {
-      cy.get("h2").should('contain', "Let's Get Started");
-    })
-
-  });
-
-  context("Weak password", () => {
-
-    beforeEach(() => {});
-
-    it("GIVEN I enter a valid email and am redirected to the home page", () => {
-      cy.visit('http://localhost:3000');
-      cy.get("form").within(() => {
-        cy.get('input[id="email"]').should('not.be.disabled').type("testing123@gmail.com");
-        cy.get('button[type="submit"]').click();
-      });
-    });
-
-    it("WHEN I input an invalid (not strong enough) password", () => {
-      cy.get("form").within(() => {
-        cy.get('input[id="firstName"]').type("Cypress");
-        cy.get('input[id="password"]').type("12345");
-        cy.get('input[id="confirmPassword"]').type("12345");
-      });
-
-    });``
-
-    it("THEN the create account button should be disabled", () => {
-      cy.get('button[type="submit"]').should("be.disabled");
-    });
 
 
 
 
     
-  })
 
 
 
-
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
 });
 
 const BACKEND_URL = "http://localhost:8000";
@@ -115,8 +92,20 @@ const BACKEND_URL = "http://localhost:8000";
 
 describe("Journal successfully is sent to DB (POST CALL)", () => {
 
+  beforeEach(() => {
+    cy.request({
+      method: 'POST',
+      url: "http://localhost:8000/api/login",
+      body: {
+        email: 'testing@gmail.com',
+        password: 'Password123$'
+      }
+    })
+
+  });
+
+
   context("Correctly input entry", () => {
-    beforeEach(() => {});
 
     it("GIVEN the user has clicked submit on a journal entry");
 
@@ -125,7 +114,8 @@ describe("Journal successfully is sent to DB (POST CALL)", () => {
         is_public: true,
         rose_text: 'Slept well',
         bud_text: 'Have to finish up patch method',
-        thorn_text: 'Tired'};
+        thorn_text: 'Tired',
+        tags: []};
 
       it("WHEN I attempt to post the entry", () => {
         cy.request({method: "POST",
@@ -149,9 +139,7 @@ describe("Journal successfully is sent to DB (POST CALL)", () => {
     let entry = {
       user_id: "efhoeihqi0ohw",
       is_public: true,
-      rose_text: 'Slept well',
-      bud_text: '',
-      thorn_text: ''};
+      rose_text: 'Slept well',};
 
       it("WHEN I attempt to post the entry", () => {
         cy.request({method: "POST",
@@ -159,7 +147,7 @@ describe("Journal successfully is sent to DB (POST CALL)", () => {
           body: entry,
           failOnStatusCode: false,
         }).then((response) => {
-          assert.equal(response.status, 500, "THEN I receive a bad response (500)");
+          assert.equal(response.status, 400, "THEN I receive a bad response (400)");
         })
       });
 
@@ -168,19 +156,31 @@ describe("Journal successfully is sent to DB (POST CALL)", () => {
 });
 
 describe("All of a users entries are succesfullly retrieved (GET)" , () => {
+
+  beforeEach(()=> {
+  })
+
   
   context("Valid user Id returns correctly", () => {
 
-    it("GIVEN the page is making a GET call for a user's entries (this could be main or entry page)");
+    it("GIVEN the page is making a GET call for a user's entries (this could be main or entry page)", () => {
+      cy.clearCookies();
+      cy.request({
+        method: 'POST',
+        url: "http://localhost:8000/api/login",
+        body: {
+          email: 'testing@gmail.com',
+          password: 'Password123$'
+        }
+      })
+    });
 
-    const userId = "67a13e0d9c8a04466bd32264";
     it("WHEN I attempt the GET call", () => {
       cy.request({method: "GET",
         url: `${BACKEND_URL}/api/entries`,
-        userId: userId,
         failOnStatusCode: false,
       }).then((response) => {
-        assert.equal(response.status, 201, "THEN I receive a successful response (201)");
+        assert.equal(response.status, 200, "THEN I receive a successful response (200)");
         assert.exists(response.body[0]._id, "AND the response entry created contains an array of entries, and the first has an _id");
       });
     });
@@ -188,18 +188,29 @@ describe("All of a users entries are succesfullly retrieved (GET)" , () => {
 
   });
 
-  context("Invalid user ID returns bad", () => {
-    it("GIVEN a bad request (in this example, a user ID which doesn't exist) for getting a user's entries");
+  context("User has no entries", () => {
+    it("GIVEN the user has no entries", () => {
+      cy.clearCookies();
+      cy.request({
+        method: 'POST',
+        url: "http://localhost:8000/api/login",
+        body: {
+          email: 'testing2@gmail.com',
+          password: 'Password123$'
+        }
+      })
+      
+    });
 
-    it("WHEN I attempt to post the entry", () => {
+    it("WHEN I attempt to view the entry", () => {
 
-      const userId = "ewihfowief";
+
       cy.request({method: "GET",
         url: `${BACKEND_URL}/api/entries`,
-        userId: userId,
         failOnStatusCode: false,
       }).then((response) => {
-        assert.equal(response.status, 500, "THEN I receive a bad response (500)");
+        assert.equal(response.status, 200, "THEN I receive a OK response (200)");
+        assert.deepEqual(response.body, [], "AND the returned array is empty");
       })
     });
 
